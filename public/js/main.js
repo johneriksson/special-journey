7c0621bca06af7d10ea6a032e44b62fa46e1358a
John Eriksson <johne3@kth.se>
2016-02-05 14:12:04 +0100

    Chat functionality done.

--- public/js/main.js
+++ public/js/main.js
@@ -1,22 +1,33 @@
+var ws;
 
 function initWs() {
     var url = "ws://localhost:60001";
-    var ws = new WebSocket(url);
+    ws = new WebSocket(url);
 
     ws.onopen = function() {
-        console.log('The websocket is now open.');
-        ws.send('Thanks for letting me connect to you.');
+        console.log("WebSocket open.");
     };
 
     ws.onmessage = function(event) {
         console.log(event.data);
+        
+        var text = $("#textarea").val();
+        $("#textarea").val(text + event.data + "\n");
     };
 
     ws.onclose = function() {
-        console.log('The websocket is now closed.');
+        console.log("WebSocket closed.");
     };   
 }
 
 $(document).ready(function() {
     initWs();
 });
+
+function send() {
+    var input = $("#input").val();
+    if(input != undefined && input != "") {
+        ws.send(input);
+        $("#input").val("");
+    }
+}
