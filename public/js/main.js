28967725b261cfdaee710de3f010abb228059938
John Eriksson <johne3@kth.se>
2016-02-14 11:15:07 +0100

    Changed colors

--- public/js/main.js
+++ public/js/main.js
@@ -1,33 +1,50 @@
-var ws;
-
-function initWs() {
-    var url = "ws://localhost:60001";
-    ws = new WebSocket(url);
-
-    ws.onopen = function() {
-        console.log("WebSocket open.");
-    };
-
-    ws.onmessage = function(event) {
-        console.log(event.data);
-        
-        var text = $("#textarea").val();
-        $("#textarea").val(text + event.data + "\n");
-    };
-
-    ws.onclose = function() {
-        console.log("WebSocket closed.");
-    };   
-}
-
-$(document).ready(function() {
-    initWs();
-});
-
-function send() {
-    var input = $("#input").val();
-    if(input != undefined && input != "") {
-        ws.send(input);
-        $("#input").val("");
+(function ($) {
+    var ws;
+    
+    function appendToChat(username, timestamp, message) {
+        $("#textarea").append("<p class='message-info'>" + username + " @ " + timestamp + "</p>");
+        $("#textarea").append("<p class='message'>" + message + "</p>");
     }
-}
+    
+    function convertTimestamp(UNIX_timestamp) {
+        var fullDate = new Date(UNIX_timestamp * 1000);
+        var hour = fullDate.getHours() < 10 ? "0" + fullDate.getHours() : fullDate.getHours();
+        var min = fullDate.getMinutes() < 10 ? "0" + fullDate.getMinutes() : fullDate.getMinutes();
+        var sec = fullDate.getSeconds() < 10 ? "0" + fullDate.getSeconds() : fullDate.getSeconds();
+        
+        var timestamp = hour + ":" + min + ":" + sec;
+    }
+    
+    function initWs() {
+        var url = "ws://localhost:60001";
+        ws = new WebSocket(url);
+        
+        ws.onopen = function () {
+            console.log("WebSocket open.");
+        };
+        
+        ws.onmessage = function (event) {
+            var timestamp = convertTimestamp(event.timestamp);
+            var username = event.username;
+            var message = event.data;
+            
+            appendToChat(username, timestamp, message);
+        };
+        
+        ws.onclose = function () {
+            console.log("WebSocket closed.");
+        };
+    }
+    
+    $(document).ready(function () {
+        initWs();
+        
+        $("#send").on("click", function() {
+            var input = $("#input").val();
+            if (input != undefined && input != "") {
+                ws.send(input);
+                $("#input").val("");
+            } 
+        });
+    });
+})(jQuery);
